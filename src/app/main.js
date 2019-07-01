d3086b6f128b01a2845d7f2c21ce02d506f9795f
Marc Guinea <develop.marcguinea@gmail.com>
2019-07-01 22:28:02 +0200

    chore: first required files

---
+++ src/app/main.js
@@ -0,0 +1,1 @@
+import { game } from './systems/game';
---
+++ src/app/systems/game.js
@@ -0,0 +1,11 @@
+AFRAME.registerSystem('game', {
+    schema: {},
+
+    init: function () {
+        console.log('Game Initialized');
+    },
+
+    tick(t, timeDelta){
+        // Your gameloop code
+    }
+});
